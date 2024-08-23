ae8a4bd19e8165ed3629593132a54e701f178c5d
rasikh11 <mo.rasikh11@gmail.com>
2024-08-23 14:47:50 +0500

    ec2_instance creation

---
+++ Day-1/ec2-instance-creation/main.tf
@@ -0,0 +1,10 @@
+```
+provider "aws" {
+	region = "us-east-1"   # Set your desired AWS region
+}
+
+resource "aws_instance" "my_instance" {
+	ami = "ami-0c55b159cbfafe1f0"   # Specify an appropriate AMI ID
+	instance_type = "t2.micro"
+}
+```
